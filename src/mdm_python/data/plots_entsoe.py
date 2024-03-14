a2fee5aa9ba5525e1402639aa06d24b3a0fda3b8
danielakomenda <d.komenda@hotmail.com>
2024-03-14 19:07:40 +0100

    refactoring entsoe-plots

--- src/mdm_python/data/plots_entsoe.py
+++ src/mdm_python/data/plots_entsoe.py
@@ -19,46 +19,45 @@ def grouped_bar_plot(data_daily):
     """Create Bar-Plots grouped by years"""
 
     data_yearly = data_daily.iloc[:, :-1].groupby(data_daily.index.year).mean()
-    data_yearly = data_yearly.reset_index().astype({"date":str}).set_index("date")
-    
+    data_yearly = data_yearly.reset_index().astype({"date": str}).set_index("date")
+
     source = bokeh.models.ColumnDataSource(data=data_yearly.reset_index())
-    
+
     color = "#BBBBBB #F0E442 #D55E00 #009E73 #0072B2 #56B4E9".split()
-    
+
     bar_plot = bokeh.plotting.figure(
         x_range=data_yearly.index.to_list(),
         y_range=(0, 3500),
-        title='Average Energy Production by Year and Source',
-        height=350, toolbar_location=None, tools="")
-    
-    
+        title="Average Energy Production by Year and Source",
+        height=350,
+        toolbar_location=None,
+        tools="",
+        y_axis_label="Energy Production [MW]",
+    )
+
     for i, col_name in enumerate(data_yearly.columns):
-    
+
         # Position of the Bars
-        x_offset = 0.7*((i+0.5)/(data_yearly.shape[1])-0.5)
-    
+        x_offset = 0.7 * ((i + 0.5) / (data_yearly.shape[1]) - 0.5)
+
         bar_plot.vbar(
             x=bokeh.transform.dodge(
-                field_name='date',
-                value=x_offset,
-                range=bar_plot.x_range
+                field_name="date", value=x_offset, range=bar_plot.x_range
             ),
             top=col_name,
             source=source,
             width=0.1,
             color=color[i],
-            legend_label=col_name
+            legend_label=col_name,
         )
-    
-    
+
     bar_plot.x_range.range_padding = 0.1
     bar_plot.xgrid.grid_line_color = None
-    bar_plot.add_layout(bar_plot.legend[0], 'right') # Legend outside of the plot
-    
+    bar_plot.add_layout(bar_plot.legend[0], "right")  # Legend outside of the plot
+
     bokeh.plotting.show(bar_plot)
 
     return bar_plot
-
 
 
 def yearly_pie_plot(data_daily):
@@ -73,45 +72,46 @@ def yearly_pie_plot(data_daily):
         years_sorted.append(year)
     years_sorted.sort()
 
-    
     for year in years_sorted:
-        
+
         yearly_results = data_daily[data_daily.index.year == year]
-        
-        x = round(yearly_results.iloc[:,:-1].sum()/1000, 2)
-        
-        data = pd.Series(x).reset_index(name='value').rename(columns={'index': 'country'})
-        data['angle'] = data['value']/data['value'].sum() * 2*math.pi
-        data['color'] = "#BBBBBB #F0E442 #D55E00 #009E73 #0072B2 #56B4E9".split()
-        
+
+        x = round(yearly_results.iloc[:, :-1].sum() / 1000, 2)
+
+        data = (
+            pd.Series(x).reset_index(name="value").rename(columns={"index": "country"})
+        )
+        data["angle"] = data["value"] / data["value"].sum() * 2 * math.pi
+        data["color"] = "#BBBBBB #F0E442 #D55E00 #009E73 #0072B2 #56B4E9".split()
+
         pie_plot = bokeh.plotting.figure(
-            height=350, 
-            title=f"Yearly Production by Energy-Type in GWh in {year}", 
+            height=350,
+            title=f"Yearly Production by Energy-Type in GWh in {year}",
             toolbar_location=None,
-            tools="hover", 
-            tooltips="@country: @value", 
+            tools="hover",
+            tooltips="@country: @value",
             x_range=(-0.5, 1.0),
         )
-        
+
         pie_plot.wedge(
-            x=0, 
-            y=1, 
+            x=0,
+            y=1,
             radius=0.4,
-            start_angle=bokeh.transform.cumsum('angle', include_zero=True), 
-            end_angle=bokeh.transform.cumsum('angle'),
-            line_color="white", 
-            fill_color='color', 
-            legend_field='country', 
+            start_angle=bokeh.transform.cumsum("angle", include_zero=True),
+            end_angle=bokeh.transform.cumsum("angle"),
+            line_color="white",
+            fill_color="color",
+            legend_field="country",
             source=data,
         )
-        
+
         pie_plot.axis.axis_label = None
         pie_plot.axis.visible = False
         pie_plot.grid.grid_line_color = None
-        
+
         fig = bokeh.layouts.column(pie_plot)
 
-        #Plot Figure in Notebook
+        # Plot Figure in Notebook
         bokeh.plotting.show(fig)
 
     return fig
@@ -120,56 +120,57 @@ def yearly_pie_plot(data_daily):
 def stacked_area_plot(data_hourly, data_daily):
     """Create an interactive Stacked-Area-Plot as a TimeSeries
     Plots can show daily or hourly data."""
-    
+
     hourly_source = bokeh.models.ColumnDataSource(data=data_hourly)
     daily_source = bokeh.models.ColumnDataSource(data=data_daily)
-    
+
     column_names = "nuclear solar wind water_reservoir water_river water_pump".split()
     colors = "#D55E00 #F0E442 #BBBBBB #009E73 #0072B2 #56B4E9".split()
-    
-    middle = len(data_hourly)//2
+
+    middle = len(data_hourly) // 2
     selection_range = 500
 
-    
     # Range-Plot
     range_plot = bokeh.plotting.figure(
-        height=300, 
-        width=800, 
-        x_axis_type="datetime", 
-        x_axis_location="above", 
-        x_range=(data_hourly.index[middle-selection_range], data_hourly.index[middle+selection_range]),
-        y_axis_label = "Energy Production [MW]",
+        height=300,
+        width=800,
+        x_axis_type="datetime",
+        x_axis_location="above",
+        x_range=(
+            data_hourly.index[middle - selection_range],
+            data_hourly.index[middle + selection_range],
+        ),
+        y_axis_label="Energy Production [MW]",
     )
-    
 
     # Range-Plot on daily base
     daily_plot = range_plot.varea_stack(
-        x='date',
+        x="date",
         stackers=column_names,
-        source=daily_source,    
+        source=daily_source,
         color=colors,
         legend_label=column_names,
     )
 
-    
     # Range-Plot on hourly base
     hourly_plot = range_plot.varea_stack(
-        x='datetime',
+        x="datetime",
         stackers=column_names,
-        source=hourly_source,    
+        source=hourly_source,
         color=colors,
         legend_label=column_names,
         visible=False,
     )
 
     range_plot.legend.location = "top_left"
-    
 
     # Plot-Selector
     # to change visibility of daily_plot and hourly_plot
-    plot_selector = bokeh.models.RadioButtonGroup(labels=["Stündlich", "Täglich"], active=1)
+    plot_selector = bokeh.models.RadioButtonGroup(
+        labels=["Stündlich", "Täglich"], active=1
+    )
     plot_selector.js_on_event(
-        "button_click", 
+        "button_click",
         bokeh.models.CustomJS(
             args=dict(
                 btn=plot_selector,
@@ -181,7 +182,8 @@ def stacked_area_plot(data_hourly, data_daily):
         let plots=[hplot, dplot]
         plots.forEach(p => p.forEach(q => {q.visible=false}))
         plots[btn.active].forEach(q => {q.visible=true})
-    """)
+    """,
+        ),
     )
 
     # Range-Tool
@@ -190,30 +192,29 @@ def stacked_area_plot(data_hourly, data_daily):
     range_tool.overlay.fill_color = "navy"
     range_tool.overlay.fill_alpha = 0.2
 
-    
     # Overview-Plot
     overview_plot = bokeh.plotting.figure(
         title="Drag the middle and edges of the selection box to change the range above",
-        height=130, 
-        width=800, 
+        height=130,
+        width=800,
         y_range=range_plot.y_range,
         x_axis_type="datetime",
         y_axis_type=None,
-        toolbar_location=None, 
+        toolbar_location=None,
     )
-    
+
     overview_plot.varea_stack(
-        x='date',
+        x="date",
         stackers=column_names,
-        source=daily_source,    
+        source=daily_source,
         color=colors,
     )
-   
+
     overview_plot.add_tools(range_tool)
-    
+
     # Create Figure
     fig = bokeh.layouts.column(plot_selector, range_plot, overview_plot)
-    
+
     # Plot Figure in Notebook
     bokeh.plotting.show(fig)
 
---
+++ src/mdm_python/data/plots_wetter2.py
@@ -0,0 +1,88 @@
+import pandas as pd
+
+import bokeh.embed
+import bokeh.io
+import bokeh.layouts
+import bokeh.models
+import bokeh.palettes
+import bokeh.plotting
+import bokeh.transform
+
+import mdm_python.data.db_wetter2 as db
+
+
+data = db.extract_daily_average_weather()
+
+
+def stacked_area_overview_plot(data):
+
+    day_source = bokeh.models.ColumnDataSource(data=data)
+    middle = data.index[len(data) // 2]
+    selection_range = pd.Timedelta("20 days")
+
+
+    # Selected-Part-Plot
+    p = bokeh.plotting.figure(
+        height=300,
+        width=800,
+        x_axis_type="datetime",
+        x_axis_location="above",
+        x_range=(middle - selection_range, middle + selection_range),
+    )
+
+    for tool in [bokeh.models.PanTool, bokeh.models.WheelZoomTool]:
+        p.select_one(tool).dimensions = "width"
+
+    p.yaxis.axis_label = 'Weather'
+
+    # Overview-Plot
+    overview = bokeh.plotting.figure(
+        title="Drag the middle and edges of the selection box to change the range above",
+        height=130,
+        width=800,
+        y_range=p.y_range,
+        x_axis_type="datetime",
+        y_axis_type=None,
+        tools="hover",
+        toolbar_location=None,
+    )
+
+    for fig in [p, overview]:
+        fig.extra_y_ranges['rain'] = bokeh.models.Range1d(0, data.rain.max()+1, bounds=(0, None))
+
+        fig.vbar(
+            x='date',
+            top="rain",
+            source=day_source,
+            color="#0072B2",
+            legend_label="Rain",
+            width=pd.Timedelta(hours=20),
+            y_range_name="rain",
+        )
+
+        fig.line(
+            x='date',
+            y="avg_temp",
+            source=day_source,
+            color="#D55E00",
+            legend_label="Temperature",
+        )
+
+    ax2 = bokeh.models.LinearAxis(y_range_name="rain", axis_label="rain [mm/h]")
+    ax2.axis_label_text_color = "navy"
+    p.add_layout(ax2, 'right')
+
+    p.legend.location = "top_left"
+
+    range_tool = bokeh.models.RangeTool(x_range=p.x_range)
+    range_tool.overlay.fill_color = "navy"
+    range_tool.overlay.fill_alpha = 0.2
+
+    overview.ygrid.grid_line_color = None
+    overview.add_tools(range_tool)
+
+    fig = bokeh.layouts.column(p, overview)
+
+    bokeh.plotting.show(fig)
+
+    return fig
